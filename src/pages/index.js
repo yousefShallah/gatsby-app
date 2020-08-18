c0ce4e5d2ffc769e734d5ecf7682659c35042d8e
yousefShallah <yousefmohshallah33@gmail.com>
2020-08-18 15:08:31 +0300

    yousef [add] create gatsby app

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
+import MainPage from './main-page';
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <MainPage />
 }
---
+++ src/pages/main-page/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MainPage extends React.Component{
+    render(){
+        return(
+            <div>
+                hello from home 
+            </div>
+        )
+    }
+}
+
+export default MainPage;
